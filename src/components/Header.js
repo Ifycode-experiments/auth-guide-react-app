500e4b13cbb8ab2f39dc183e109ed2a1da09c6b5
Ifycode <marychord@gmail.com>
2021-11-13 07:39:42 +0100

    Hide or show navBtns depending on nav visibility

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,18 @@ import { Component } from 'react';
 
 class Header extends Component {
     state = {
-        hidden: true
+        hidden: true,
+        visible: true
     };
 
     toggleNav = e => {
+        //For hiding or showing nav element
         const currentState = this.state.hidden;
         this.setState({ hidden: !currentState });
+
+        //For hiding or showing <NavBtn />
+        const currentVisibleState = this.state.visible;
+        this.setState({ visible: !currentVisibleState });
     }
 
     render() {
@@ -21,9 +27,15 @@ class Header extends Component {
                     <a className="brand-logo" href="./">
                         <img src='./images/logo/auth-logo-colored-2.svg' alt="auth guide app logo" />
                     </a>
-                    <NavBtn toggleNav={this.toggleNav} />
+                    <NavBtn 
+                        toggleNav={this.toggleNav}
+                        visible={this.state.visible} 
+                    />
                     <nav className={`nav nav-show ${this.state.hidden ? 'nav-hide' : '' }`}>
-                        <NavBtn toggleNav={this.toggleNav} />
+                        <NavBtn 
+                            toggleNav={this.toggleNav} 
+                            visible={!this.state.visible}
+                        />
                         <ul className="menu-container">
                             {Object.keys(linkDetails).map(linkKey => 
                                 <NavLink 
--- src/components/NavBtn.js
+++ src/components/NavBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/nav/show-on-ss.css';
 
 const NavBtn = (props) => (
-    <button onClick={props.toggleNav} className="show-on-ss">Menu</button>
+    <button onClick={props.toggleNav} className={`show-on-ss ${props.visible ? '' : 'hidden'}`}>Menu</button>
 );
 
 export default NavBtn;
