02ea55eb29a2ad415a43f2a4f20c121b4bb7a444
Ifycode <marychord@gmail.com>
2021-11-13 02:38:00 +0100

    Move articles into GuideIntro component

--- src/components/AuthHome.js
+++ src/components/AuthHome.js
@@ -1,10 +1,9 @@
 import { Component, Fragment } from 'react';
 import Header from './Header';
+import GuideIntro from './GuideIntro';
+import guideintro from '../guideintro';
 import '../css/flexie.css';
 import '../css/grid.css';
-import docIcon from '../images/icon/doc.svg';
-import authusersIcon from '../images/icon/auth-users.svg';
-import adminIcon from '../images/icon/admin.svg';
 
 class AuthHome extends Component {
   render() {
@@ -13,33 +12,11 @@ class AuthHome extends Component {
         <Header />
         <section className="contain">
           <div className="g g-sb-ls">
-            <article className="intro">
-              <div className="icon">
-                <img src={docIcon} alt="doc icon" />
-              </div>
-              <div>
-                <h1>Content</h1>
-                <p>Add what content is in the auth guide here - doc + tutorial</p>
-              </div>
-            </article>
-            <article className="intro">
-              <div className="icon">
-                <img src={authusersIcon} alt="auth users icon" />
-              </div>
-              <div>
-                <h1>Access</h1>
-                <p>Who can access what content, what route and read and write access</p>
-              </div>
-            </article>
-            <article className="intro">
-              <div className="icon">
-                <img src={adminIcon} alt="admin icon" />
-              </div>
-              <div>
-                <h1>Not yet determined</h1>
-                <p>This title and content is not yet determined</p>
-              </div>
-            </article>
+            { guideintro.map(intro => 
+              <GuideIntro 
+                key={intro.title.toLowerCase()}
+                details={intro} />
+            )}
           </div>
         </section>
         {/* The Doc section will be added here later on */}
---
+++ src/components/GuideIntro.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+const GuideIntro = (props) => (
+   <Fragment>
+       <article className="intro">
+            <div className="icon">
+                <img src={props.details.image.url} alt={props.details.image.alt} />
+            </div>
+            <div>
+                <h1>{props.details.title}</h1>
+                <p>{props.details.paragraph}</p>
+            </div>
+        </article>
+   </Fragment>
+);
+
+export default GuideIntro;
