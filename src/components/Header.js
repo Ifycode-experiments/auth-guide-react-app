9583ccbc02fe3edd0d7af89ef11a7fb4cc6d3c7e
Ifycode <marychord@gmail.com>
2021-11-13 06:39:51 +0100

    Toggle nav for smaller screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,41 @@ import NavBtn from './NavBtn';
 import '../css/header.css';
 import linkDetails from '../navlinks';
 import NavLink from './NavLink';
+import { Component } from 'react';
 
-const Header = () => (
-    <header>
-        <div className="contain f-sb-ai-center" >
-            <a className="brand-logo" href="./">
-                <img src='./images/logo/auth-logo-colored-2.svg' alt="auth guide app logo" />
-            </a>
-            <NavBtn />
-            <nav className="nav nav-hide nav-show">
-                <NavBtn />
-                <ul className="menu-container">
-                    {Object.keys(linkDetails).map(linkKey => 
-                        <NavLink 
-                            key={linkKey} 
-                            details={linkDetails[linkKey]}
-                        /> 
-                    )}
-                </ul>
-            </nav>
-        </div>
-    </header>
-);
+class Header extends Component {
+    state = {
+        hidden: true
+    };
+
+    toggleNav = e => {
+        const currentState = this.state.hidden;
+        this.setState({ hidden: !currentState });
+    }
+
+    render() {
+        return (
+            <header>
+                <div className="contain f-sb-ai-center" >
+                    <a className="brand-logo" href="./">
+                        <img src='./images/logo/auth-logo-colored-2.svg' alt="auth guide app logo" />
+                    </a>
+                    <NavBtn toggleNav={this.toggleNav} />
+                    <nav className={`nav nav-show ${this.state.hidden ? 'nav-hide' : '' }`}>
+                        <NavBtn toggleNav={this.toggleNav} />
+                        <ul className="menu-container">
+                            {Object.keys(linkDetails).map(linkKey => 
+                                <NavLink 
+                                    key={linkKey} 
+                                    details={linkDetails[linkKey]}
+                                /> 
+                            )}
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        );
+    }
+}
 
 export default Header;
--- src/components/NavBtn.js
+++ src/components/NavBtn.js
@@ -1,16 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/nav/show-on-ss.css';
 
-class NavBtn extends Component {
-    headerButton = React.createRef();
-    toggleNav = e => {
-        console.log(this.headerButton);
-    }
-    render() {
-        return (
-            <button ref={this.headerButton} onClick={this.toggleNav} className="show-on-ss">Menu</button>
-        )
-    }
-};
+const NavBtn = (props) => (
+    <button onClick={props.toggleNav} className="show-on-ss">Menu</button>
+);
 
 export default NavBtn;
