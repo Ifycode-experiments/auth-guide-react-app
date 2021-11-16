be13d15b61ccb41eb24f8fa405661d2bd9fc23d3
Ifycode <marychord@gmail.com>
2021-11-16 02:21:34 +0100

    Clean up spaces in styles, config and helper js

--- src/helper/docintro.js
+++ src/helper/docintro.js
@@ -1,30 +1,29 @@
 
 const intro = [
-    {
-        title: 'Content',
-        paragraph: 'Add what content is in the auth guide here - doc + tutorial',
-        image: {
-            url: './images/icon/doc.svg',
-            alt: 'doc icon'
-        }
-    },
-    {
-        title: 'Access',
-        paragraph: 'Who can access what content, what route and read and write access',
-        image: {
-            url: './images/icon/auth-users.svg',
-            alt: 'auth users icon'
-        }
-    },
-    {
-        title: 'Not yet determined',
-        paragraph: 'This title and content is not yet determined',
-        image: {
-            url: './images/icon/admin.svg',
-            alt: 'admin icon'
-        }
-    },
+  {
+    title: 'Content',
+    paragraph: 'Add what content is in the auth guide here - doc + tutorial',
+    image: {
+      url: './images/icon/doc.svg',
+      alt: 'doc icon'
+    }
+  },
+  {
+    title: 'Access',
+    paragraph: 'Who can access what content, what route and read and write access',
+    image: {
+      url: './images/icon/auth-users.svg',
+      alt: 'auth users icon'
+    }
+  },
+  {
+    title: 'Not yet determined',
+    paragraph: 'This title and content is not yet determined',
+    image: {
+      url: './images/icon/admin.svg',
+      alt: 'admin icon'
+    }
+  },
 ];
-  
+
 export default intro;
-  
